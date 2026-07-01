3ff9dada82750ce284a26889dabff3a087b268ff
yash321-hub <yashwanthreddy980@gmail.com>
2026-07-01 23:11:18 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,16 @@
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello Selenium");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
     }
 }
